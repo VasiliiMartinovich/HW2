d6c502e0166bbd2ad00ddd5856cb2adcaec70285
Vasilii Martinovich <martinovichvasilii@gmail.com>
2026-05-27 14:36:01 +0300

    Add HW2.cs

--- HW2.cs
+++ HW2.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter your name:");
+        var name = Console.ReadLine();
+        Console.WriteLine(name);
     }
 }
