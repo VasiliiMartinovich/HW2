f0818020fca7270eb1b07b17c9bcaadc230ebcfa
Vasilii Martinovich <martinovichvasilii@gmail.com>
2026-05-27 14:29:43 +0300

    remove code

--- HW2.cs
+++ HW2.cs
@@ -5,7 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter your name:");
-        var name = Console.ReadLine();
-        Console.WriteLine(name);
     }
 }
